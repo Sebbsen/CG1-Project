8ec88d2d45913e049a111810bec38885b9d79e01
Sebastian <sebastiangremm97@gmail.com>
2024-06-23 16:44:14 +0200

    remove sebbo-test_area

--- sebbo-test_area/shaders/fragmentShader.glsl
+++
@@ -1,6 +0,0 @@
-precision mediump float; // Präzisionsspezifikator hinzufügen
-varying vec3 fragColor; // von Vertex Shader erhalten
-
-void main() {
-    gl_FragColor = vec4(fragColor, 1.0); // setzen Sie die Farbe des Fragments
-}
